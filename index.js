566d858eb351f3996823659c1507b85f77282a54
Mohammad Shohag <shohagcsediu@gmail.com>
2021-06-21 12:47:20 +0600

    add insert api added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(fileUpload());
 
 const port = process.env.PORT || 5000;
 
+// Server Homepage
 app.get('/', (req, res) => {
     res.send("Welcome To Job Monster Server")
 })
@@ -23,7 +24,52 @@ app.get('/', (req, res) => {
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     console.log("database connection errors:", err);
-    const jobCollection = client.db("jobmonster").collection("jobs");
+    const jobsCollection = client.db("jobmonster").collection("jobs");
+    const adminCollection = client.db("jobmonster").collection("admin");
+    const employerCollection = client.db("jobmonster").collection("employer");
+    const seekerCollection = client.db("jobmonster").collection("seeker");
+
+    // Add Job Api Insert
+    app.post('/addjob/', (req, res) => {
+        const jobs = req.body;
+        console.log(jobs);
+        jobsCollection.insertOne(jobs)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+      })
+
+    // Add Admin Api Insert
+    app.post('/addadmin/', (req, res) => {
+        const admin = req.body;
+        console.log(admin);
+        adminCollection.insertOne(admin)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+      })
+
+    // Add Employer Api Insert
+    app.post('/addjob/', (req, res) => {
+        const employer = req.body;
+        console.log(employer);
+        employerCollection.insertOne(employer)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+      })
+
+    // Add Job Seeker Api Insert
+    app.post('/addseeker/', (req, res) => {
+        const seeker = req.body;
+        console.log(seeker);
+        seekerCollection.insertOne(seeker)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+      })
+
+
 
 
 });
